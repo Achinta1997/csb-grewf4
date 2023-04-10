4c1895e0a28504f4545b05e47e885b90edac0cef
Code Sandbox <hello@codesandbox.io>
2023-04-10 21:34:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b724096bef231ef1f707ae181c940e56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695bac493361b491cfa524e1eabb7f6b",
+    "url": "https://achinta1997.github.io/csb-grewf4/index.html"
+  },
+  {
+    "revision": "ee9e355d3438769b7034",
+    "url": "https://achinta1997.github.io/csb-grewf4/static/js/2.88831c34.chunk.js"
+  },
+  {
+    "revision": "d45d7478908bb294690c",
+    "url": "https://achinta1997.github.io/csb-grewf4/static/js/main.b0db9bbf.chunk.js"
+  },
+  {
+    "revision": "8fc3dd0c4556fa19ac70",
+    "url": "https://achinta1997.github.io/csb-grewf4/static/js/runtime-main.7bd92a6d.js"
+  }
+]);
